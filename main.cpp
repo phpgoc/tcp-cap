8175e3b1d345cb5810e646593ab20508e94b8947
phpgoc <phpgoc@gmail.com>
2022-09-30 11:17:23 +0800

    serde rawPacket

--- main.cpp
+++ main.cpp
@@ -7,11 +7,22 @@ using namespace std;
 #include "SystemUtils.h"
 static void onPacketArrives(pcpp::RawPacket *packet, pcpp::PcapLiveDevice *dev, void *cookie) {
     // extract the stats object form the cookie
-
+    server_drvier::Base *server = (server_drvier::Base *) cookie;
     // parsed the raw packet
     pcpp::Packet parsedPacket(packet);
-    cout << parsedPacket.toString() << endl;
-    // print packet info
+    //    cout << parsedPacket.toString() << endl;
+    //    server->push(parsedPacket.toString());
+    //todo convert packet to byte which can be send to server
+    char *b = (char *) malloc(sizeof(int) + sizeof(timespec) + parsedPacket.getRawPacket()->getRawDataLen());
+
+    char *p = b;
+    *((int *) p) = packet->getRawDataLen();
+    p += sizeof(int);
+    *((timespec *) p) = packet->getPacketTimeStamp();
+    p += sizeof(timespec);
+    memcpy(p, packet, packet->getRawDataLen());
+    server->push(string(b, sizeof(int) + sizeof(timespec) + packet->getRawDataLen()));
+    free(b);
 }
 int main(int argc, char *argv[]) {
     Config c;
@@ -33,19 +44,31 @@ int main(int argc, char *argv[]) {
     //        sleep(1);
     //    }
     pcpp::OrFilter OrFilter;
+    vector<void *> filterPointersForDeletion;
     for (auto i : c.getMDbs()) {
         // create a filter instance to capture only traffic on port 80
         pcpp::PortFilter *portFilter = new pcpp::PortFilter(i.first, pcpp::SRC_OR_DST);
-
+        filterPointersForDeletion.push_back(portFilter);
         OrFilter.addFilter(portFilter);
     }
     pcpp::AndFilter AndFilter;
     AndFilter.addFilter(&OrFilter);
     pcpp::ProtoFilter protocolFilter(pcpp::TCP);
     AndFilter.addFilter(&protocolFilter);
+
     c.getMDevice()->setFilter(AndFilter);
-    c.getMDevice()->startCapture(onPacketArrives, nullptr);
+    c.getMDevice()->startCapture(onPacketArrives, server);
+    auto safe_quit_process = [](int) {
+        //        c.getMDevice()->stopCapture();
+        //        for (auto i : filterPointersForDeletion) {
+        //            delete (pcpp::PortFilter *) i;
+        //        }
+        //        delete server;
+        printf("safe quit");
+    };
+    signal(SIGTERM, safe_quit_process);
+    signal(SIGINT, safe_quit_process);
     pcpp::multiPlatformSleep(2147483647);
-    delete server;
+
     return 0;
 }
